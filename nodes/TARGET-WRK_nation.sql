3ad07344fab1c7fd932d8e00132b26e086babcac
DE-Tanvi <tanvi.kamat@dataentrega.com>
2026-08-25 16:28:56 +0530

    Added TARGET-WRK nation node

--- nodes/TARGET-WRK_nation.sql
+++ nodes/TARGET-WRK_nation.sql
@@ -1,9 +1,9 @@
 @id("f602cfcd-87c3-432e-acbf-b6fd8cd05189")
 @nodeType("705")
 SELECT
-     `N_NATIONKEY` AS `N_NATIONKEY`,
-     `N_NAME` AS `N_NAME`,
-     `N_REGIONKEY` AS `N_REGIONKEY`,
+     `N_NATIONKEY` AS `N_NATIONKEY` @notNull @defaultValue("0"),
+     `N_NAME` AS `N_NAME` @description("Name") @notNull @defaultValue("NA"),
+     `N_REGIONKEY` AS `N_REGIONKEY` @notNull,
      `N_COMMENT` AS `N_COMMENT`,
-     `last_modified` AS `last_modified`
+     `last_modified` AS `last_modified` @description("TimeStamp")
 FROM {{ ref('SRC', 'nation') }} `nation`
