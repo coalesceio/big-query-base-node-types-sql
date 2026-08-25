a59fa4b4b122ce6581be69008d21ed3cc29de9b7
prj2001-udn <prj2001.github@dataentrega.com>
2026-08-25 20:05:47 +0530

    Update TARGET-WRK nation node-3

--- nodes/TARGET-WRK_nation.sql
+++ nodes/TARGET-WRK_nation.sql
@@ -11,11 +11,11 @@
 @preSQL("SELECT 1 FROM {{ this }} GROUP BY N_COMMENT HAVING COUNT(*) > 3")
 @postSQL("SELECT 1 FROM {{ this }} GROUP BY N_COMMENT HAVING COUNT(*) > 4")
 SELECT
-     "N_NATIONKEY" AS "N_NATIONKEY" @tests("unique") @inHash("GH_COL1", 2),
-     "N_NAME" AS "N_NAME" @inHash("GH_COL2", 1),
-     "N_REGIONKEY" AS "N_REGIONKEY"  @tests("null") @tests("unique") @inHash("GH_COL1", 1) @inHash("GH_COL2", 2),
-     "N_COMMENT" AS "N_COMMENT" @description("'Nation Comment'") @defaultValue("'NA'") @notNull,
-     "N_LOAD_TIMESTAMP" AS "N_LOAD_TIMESTAMP" @tests("null") @defaultValue("current_timestamp()"),
-     {{ get_hash('GH_COL1') }}::STRING AS "GH_COL1" @defaultValue("NULL"),
-     {{ get_hash('GH_COL2') }}::STRING AS "GH_COL2"
-FROM {{ ref('SOURCE_DATA', 'NATION_TEST') }} "NATION_TEST"
+     `N_NATIONKEY` AS `N_NATIONKEY` @tests("unique") @inHash("GH_COL1", 2),
+     `N_NAME` AS `N_NAME` @description("Name")@inHash("GH_COL2", 1),
+     `N_REGIONKEY` AS `N_REGIONKEY` @tests("null") @tests("unique") @inHash("GH_COL1", 1) @inHash("GH_COL2", 2),
+     `N_COMMENT` AS `N_COMMENT` @description("'Nation Comment'") @defaultValue("'NA'") @notNull,
+     `last_modified` AS `last_modified` @tests("null") @defaultValue("current_timestamp()"),
+     CAST({{ get_hash('GH_COL1') }} AS STRING) AS "GH_COL1" @defaultValue("NULL"),
+     CAST({{ get_hash('GH_COL2') }}AS STRING) AS "GH_COL2"
+FROM {{ ref('SRC', 'nation') }} `nation`
