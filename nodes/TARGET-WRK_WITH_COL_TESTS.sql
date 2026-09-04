b5d536098e6eaeef4b83e2796a6bb842b1727f40
Tanvi Kamat <tanvi.kamat.contractor@coalesce.io>
2026-09-04 16:20:10 +0530

    description test

--- nodes/TARGET-WRK_WITH_COL_TESTS.sql
+++ nodes/TARGET-WRK_WITH_COL_TESTS.sql
@@ -7,11 +7,11 @@
 @preSQL("SELECT 1 FROM {{ this }} GROUP BY N_NAME HAVING COUNT(*) > 1")
 @postSQL("SELECT 1 FROM {{ this }} GROUP BY N_NAME HAVING COUNT(*) > 2")
 SELECT ALL
-     `N_NATIONKEY` AS `N_NATIONKEY` @uniqueness @min_value("0") @max_value("100")  @accepted_values("1") @inHash("GH_COL1", 2) @defaultValue("20") @description("primary column"),
-     `N_NAME` AS `N_NAME` @empty @accepted_values("'ALGERIA'") @accepted_values("'ARGENTINA'") @inHash("GH_COL1", 1) @defaultValue("'20'"),
+     `N_NATIONKEY` AS `N_NATIONKEY` @uniqueness @min_value("0") @max_value("100")  @accepted_values("1") @inHash("GH_COL1", 2) @defaultValue("20") @description("primary column - 1"),
+     `N_NAME` AS `N_NAME` @empty @accepted_values("'ALGERIA'") @accepted_values("'ARGENTINA'") @inHash("GH_COL1", 1) @defaultValue("'20'") @description("primary column - 2"),
      `N_REGIONKEY` AS `N_REGIONKEY` @min_max("0", "4"),
      `N_COMMENT` AS `N_COMMENT` @rejected_values("'NA'") @not_null,
-     `last_modified` AS L_M_1 @freshness(7, "DAY") @relative_time("<", "L_M_2") @description("timestamp column"),
+     `last_modified` AS L_M_1 @freshness(7, "DAY") @relative_time("<", "L_M_2"),
      `last_modified` AS L_M_2,
      CAST({{ get_hash('GH_COL1') }} AS STRING) AS `GH_COL1` @description("Hash Column"),
 FROM {{ ref('SRC', 'nation') }} `nation`
