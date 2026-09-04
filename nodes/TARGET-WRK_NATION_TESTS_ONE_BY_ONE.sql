790b06a6af77b0719318bb872692fa1003737d2e
Tanvi Kamat <tanvi.kamat.contractor@coalesce.io>
2026-09-04 15:02:52 +0530

    View -> Table

--- nodes/TARGET-WRK_NATION_TESTS_ONE_BY_ONE.sql
+++ nodes/TARGET-WRK_NATION_TESTS_ONE_BY_ONE.sql
@@ -1,7 +1,7 @@
 @id("42692b52-8d76-4fe6-b2a4-06161e559261")
 @nodeType("705")
 @disableTests
-@materializationType("view")
+
 @writeMode("append  ")
 
 -- preSQL/postSQL with backtick-quoted identifiers inside the double-quoted string
