cbd98746c32694d305e673df5292eb98a1ae7ef3
Tanvi Kamat <tanvi.kamat.contractor@coalesce.io>
2026-09-04 16:07:00 +0530

    set notNull - cancelled

--- nodes/TARGET-WRK_WITH_COL_TESTS.sql
+++ nodes/TARGET-WRK_WITH_COL_TESTS.sql
@@ -8,7 +8,7 @@
 @postSQL("SELECT 1 FROM {{ this }} GROUP BY N_NAME HAVING COUNT(*) > 2")
 SELECT ALL
      `N_NATIONKEY` AS `N_NATIONKEY` @uniqueness @min_value("0") @max_value("100")  @accepted_values("1") @inHash("GH_COL1", 2) @defaultValue("10") @description("primary column"),
-     `N_NAME` AS `N_NAME` @empty @accepted_values("'ALGERIA'") @accepted_values("'ARGENTINA'") @inHash("GH_COL1", 1) @notNull,
+     `N_NAME` AS `N_NAME` @empty @accepted_values("'ALGERIA'") @accepted_values("'ARGENTINA'") @inHash("GH_COL1", 1),
      `N_REGIONKEY` AS `N_REGIONKEY` @min_max("0", "4"),
      `N_COMMENT` AS `N_COMMENT` @rejected_values("'NA'") @not_null @defaultValue("'20'"),
      `last_modified` AS L_M_1 @freshness(7, "DAY") @relative_time("<", "L_M_2") @description("timestamp column"),
