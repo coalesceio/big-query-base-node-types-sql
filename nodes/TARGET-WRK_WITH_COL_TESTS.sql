040e0ce09d502e7622fab3e6ad2d1aed5a65ae9b
Tanvi Kamat <tanvi.kamat.contractor@coalesce.io>
2026-09-04 15:54:34 +0530

    deployment

--- nodes/TARGET-WRK_WITH_COL_TESTS.sql
+++ nodes/TARGET-WRK_WITH_COL_TESTS.sql
@@ -9,14 +9,10 @@
 SELECT ALL
      `N_NATIONKEY` AS `N_NATIONKEY` @uniqueness @min_value("0") @max_value("100")  @accepted_values("1") @inHash("GH_COL1", 2) @defaultValue("10") @description("primary column") @notNull,
      `N_NAME` AS `N_NAME` @empty @accepted_values("'ALGERIA'") @accepted_values("'ARGENTINA'") @inHash("GH_COL1", 1),
-     `N_REGIONKEY` AS `N_REGIONKEY` @min_max("0", "4") @notNull,
+     `N_REGIONKEY` AS `N_REGIONKEY` @min_max("0", "4"),
      `N_COMMENT` AS `N_COMMENT` @rejected_values("'NA'") @not_null @defaultValue("'20'"),
      `last_modified` AS L_M_1 @freshness(7, "DAY") @relative_time("<", "L_M_2") @description("timestamp column"),
      `last_modified` AS L_M_2,
-     `N_NAME` AS N_NAME_1,
-     `N_NAME` AS N_NAME_2  @not_null,
-     `N_NAME` AS N_NAME_3 @not_null,
-     `N_NAME` AS N_NAME_4 @description("<text>"),
      CAST({{ get_hash('GH_COL1') }} AS STRING) AS `GH_COL1` @description("Hash Column"),
 FROM {{ ref('SRC', 'nation') }} `nation`
 WHERE N_REGIONKEY IS NOT NULL
