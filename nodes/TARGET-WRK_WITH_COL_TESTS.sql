b0a1da2759cea46f716d2f95f026576a6e24a906
Tanvi Kamat <tanvi.kamat.contractor@coalesce.io>
2026-09-04 15:15:29 +0530

    COL ADD and ALTEER TEST

--- nodes/TARGET-WRK_WITH_COL_TESTS.sql
+++ nodes/TARGET-WRK_WITH_COL_TESTS.sql
@@ -1,6 +1,5 @@
 @id("7986e87a-1377-4bdb-9fec-fa0575d17834")
 @nodeType("705")
-@disableTests
 @writeMode("append")
 @tests("SELECT 1 FROM {{ this }}")
 @tests("SELECT 2 FROM {{ this }}", true, "Before")
@@ -8,12 +7,16 @@
 @preSQL("SELECT 1 FROM {{ this }} GROUP BY N_NAME HAVING COUNT(*) > 1")
 @postSQL("SELECT 1 FROM {{ this }} GROUP BY N_NAME HAVING COUNT(*) > 2")
 SELECT ALL
-     `N_NATIONKEY` AS `N_NATIONKEY` @uniqueness @min_value("0") @max_value("100")  @accepted_values("1") @inHash("GH_COL1", 2) @defaultValue("20") @description("primary column") @notNull,
-     `N_NAME` AS `N_NAME` @not_null @empty @accepted_values("'ALGERIA'") @accepted_values("'ARGENTINA'") @inHash("GH_COL1", 1),
-     `N_REGIONKEY` AS `N_REGIONKEY` @min_max("0", "4") @notNull @defaultValue("20"),
-     `N_COMMENT` AS `N_COMMENT` @rejected_values("'NA'"),
+     `N_NATIONKEY` AS `N_NATIONKEY` @uniqueness @min_value("0") @max_value("100")  @accepted_values("1") @inHash("GH_COL1", 2) @defaultValue("10") @description("primary column") @notNull,
+     `N_NAME` AS `N_NAME` @empty @accepted_values("'ALGERIA'") @accepted_values("'ARGENTINA'") @inHash("GH_COL1", 1),
+     `N_REGIONKEY` AS `N_REGIONKEY` @min_max("0", "4") @notNull,
+     `N_COMMENT` AS `N_COMMENT` @rejected_values("'NA'") @not_null @defaultValue("'20'"),
      `last_modified` AS L_M_1 @freshness(7, "DAY") @relative_time("<", "L_M_2") @description("timestamp column"),
      `last_modified` AS L_M_2,
-     CAST({{ get_hash('GH_COL1') }} AS STRING) AS `GH_COL1` @description("Hash Column")
+     `N_NAME` AS N_NAME_1,
+     `N_NAME` AS N_NAME_2 @defaultValue("'NA'"),
+     `N_NAME` AS N_NAME_3 @not_null,
+     `N_NAME` AS N_NAME_4 @description("<text>"),
+     CAST({{ get_hash('GH_COL1') }} AS STRING) AS `GH_COL1` @description("Hash Column"),
 FROM {{ ref('SRC', 'nation') }} `nation`
 WHERE N_REGIONKEY IS NOT NULL
