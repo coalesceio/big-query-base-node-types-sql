0f6ad338022583b46dcf51e7552a1818fc3f32fa
Tanvi Kamat <tanvi.kamat.contractor@coalesce.io>
2026-09-07 18:06:27 +0530

    First commit after moving files from branch - 07/09/2026

---
+++ nodes/TARGET-WRK_nation_v2_TABLE.sql
@@ -0,0 +1,9 @@
+@id("5b141e63-4dd9-4da3-aefa-02c0b5f5687c")
+@nodeType("705")
+SELECT
+     `N_NATIONKEY` AS `N_NATIONKEY`,
+     `N_NAME` AS `N_NAME`,
+     `N_REGIONKEY` AS `N_REGIONKEY`,
+     `N_COMMENT` AS `N_COMMENT`,
+     `last_modified` AS `last_modified`
+FROM {{ ref('SRC', 'wrk_nation_v1_VIEW_RENAMED') }} `wrk_nation_v1_VIEW`
---
+++ nodes/TARGET-WRK_nation_v2_VIEW.sql
@@ -0,0 +1,10 @@
+@id("7c072b64-e563-47d9-bb3c-c9fae55aee0b")
+@nodeType("705")
+@materializationType("view")
+SELECT
+     `N_NATIONKEY` AS `N_NATIONKEY`,
+     `N_NAME` AS `N_NAME`,
+     `N_REGIONKEY` AS `N_REGIONKEY`,
+     `N_COMMENT` AS `N_COMMENT`,
+     `last_modified` AS `last_modified`
+FROM {{ ref('SRC', 'wrk_nation_v1_VIEW_RENAMED') }} `wrk_nation_v1_VIEW`
